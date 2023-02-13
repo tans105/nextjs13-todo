a3d441f027e5d463c93c076d7be014cc508d1885
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2023-02-13 12:53:43 +0530

    feat: boilerplate changes

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header className="p-5 bg-blue-500">
+      <div className="font-bold text-white">This is a header</div>
+    </header>
+  );
+}
+
+export default Header;
